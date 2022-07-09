da7e3b9b0586e289df71247bd1faf29168ce09ae
SooHyun <dame95@naver.com>
2022-07-09 16:27:13 +0000

    add spring gradle project

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+
+package com.example.demo;
+import java.text.Format;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	
+	public static void main(String[] args) {
+	SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
